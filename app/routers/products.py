5318668929f21e354efb85a77aee8f0aaec36351
pradeeptomer4u <pradeeptomer4u@gmail.com>
2025-10-25 13:03:45 +0530

    changes

--- app/routers/products.py
+++ app/routers/products.py
@@ -383,7 +383,7 @@ async def read_product(product_id: int, db: Session = Depends(get_db)):
         except:
             product.seller.business_address = {}
     
-    return schemas.Product.model_validate(product)
+    return jsonable_encoder(product)
 
 @router.put("/{product_id}", response_model=schemas.Product)
 async def update_product(
